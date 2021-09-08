e70bfc5b63a4aea581251f9b738ee3e32d50da22
mridha-emran <bari.lala490@gmail.com>
2021-09-08 15:28:30 +0200

    mogoDB add

--- Upload/frontend/upload/src/App.js
+++ Upload/frontend/upload/src/App.js
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [image, setImage] = useState();
    const[user,setUser] = useState()
-  const send = () => {
+   const[userList,setUserList]=useState([])
+
+ useEffect(()=>{
+  fetch("http://localhost:3000/ user" )
+  .then((res) => res.json())
+  .then((res) => {
+    setUserList(res.userList);
+  });
+
+ },[])
+
+  const send = (e) => {
+    e.preventDefault()
     const formData = new FormData();
     formData.append("image", image);
     fetch(`http://localhost:3000/user?username=${user}`, {
       method: "POST",
       body: formData,
     });
+    setUserList([...userList, { name: user }])
   };
-
+  console.log(user)
   return (
+    
     <>
+          <div>
+                 userList.map((elem) => {
+                  <p>{elem.name}</p>})
+              
+             
+            </div>
+         
       <input type="text" onChange={(e)=> setUser(e.target.value)} />
       <input
         type="file"
--- Upload/server.js
+++ Upload/server.js
@@ -4,22 +4,45 @@ const multer  = require('multer');
 const fs = require("fs");
 const path = require("path");
 const upload = multer({ dest: 'public/uploads/' });
+const dotenv=require("dotenv")
+dotenv.config({
+  path:"./config.env",
+})
 
-const PORT=3000;
+const mongoose=require("mongoose")
 
 const app= express();
+mongoose.connect(process.env.DB,{useNewUrlParser:true,}).then(()=>{
+  console.log("Conected to mongoDB")
+})
 
 app.use(cors())
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    res.json({status :'ok',})
+
+const userListShema = new mongoose.Schema({
+   photo:String,
+   username:String
 })
 
-app.post('/user', upload.single('image'),  (req, res) => {
-    console.log(req.file);
+const UserList = mongoose.model('UserList', userListShema)
+
+app.get("/",async (req, res) => {
+  const user=await UserList.find()
+    res.json({status :'ok',
+              data:user
+  })
+})
+
+app.post('/user', upload.single('image'), async (req, res) => {
+    console.log( "req user",req.file);
     fs.renameSync(req.file.path, path.join(req.file.destination, req.file.originalname));
+    const username=await UserList.create({
+      username:req.query.username,
+      photo:`uploads/${req.file.originalname}`
+    }) 
+    console.log( "postuserName",username)
     res.json("ok");
   });
 
-app.listen(PORT,()=>( console.log(`this server listing port:${PORT}`)))
+  app.listen( process.env.PORT,()=>( console.log("this server listing port:3000")))
