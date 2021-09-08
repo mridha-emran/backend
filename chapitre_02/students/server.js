773bd2a591578a4a46b4ac1bed24bfddb3257f73
mridha-emran <bari.lala490@gmail.com>
2021-09-08 13:33:03 +0200

    students mongoDB add

--- chapitre_02/students/server.js
+++ chapitre_02/students/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app= express();
 const PORT=3000;
-
-
+const dotenv=require("dotenv")
+dotenv.config({
+    path:"./config.env"
+})
+const mongoose=require("mongoose")
 const students = [
     "yangchent",
     "emran",
@@ -18,17 +21,36 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get("/students", (req, res) => {
-    res.json(students)
+const StudentSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	}
+});
+
+const Student = mongoose.model('Student',StudentSchema)
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true,})
+	.then(() => {
+		console.log("Connected to MongoDB !");
+	});
+
+app.get("/students", async(req, res) => {
+    const superHeroe=await Student.find()
+    res.json({
+         message:"ok",
+         data: Student
+    })
 })
 
-app.post("/students", (req, res) => {
-    const addStudents = req.body
-    students.push(addStudents)
+
+app.post("/students", async (req, res) => {
+    const addStudents = await Student.create (req.body)
+   
      console.log(addStudents)
     res.json({
         message: "Student Added",
-        addStudents
+        data: addStudents
     })
 })
 
